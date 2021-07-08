11feb8e874c4e3702eeab2988808dc911f4c3202
Szczepan86 <szczepanski.mateusz@gmail.com>
2021-07-08 19:45:59 +0200

    Sudoku printing added

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ def get_sudoku(filename):
     return None
 
 
+def print_sudoku(sudoku):
+    for line_nb, line in enumerate(sudoku):
+        if line_nb % 3 == 0 and line_nb < len(line) and line_nb:
+            print(21 * '-')
+        for col_nb, element in enumerate(line):
+            if col_nb % 3 == 0 and col_nb < len(line) and col_nb:
+                print('|', end=' ')
+            if element == '0':
+                element = '.'
+            print(element, end=' ')
+        print()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     sudoku = get_sudoku('tables\\sudoku01.txt')
-    print(sudoku)
+    print_sudoku(sudoku)
