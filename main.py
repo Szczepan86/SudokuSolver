1331774d0fb43eaf3a5b65c0200c391dfa4f6a2a
Szczepan86 <szczepanski.mateusz@gmail.com>
2021-07-08 23:19:26 +0200

    Bugfixing pt 2

--- main.py
+++ main.py
@@ -25,13 +25,16 @@ class SudokuSolver:
             for y in range(line_nb // 3 * 3, line_nb // 3 * 3 + 3):
                 for x in range(col_nb // 3 * 3, col_nb // 3 * 3 + 3):
                     self.candidates[y][x].discard(element)
-            # remove all candidates when digit is known
-            self.candidates[line_nb][col_nb] = set()
+            # remove all other candidates when digit is known
+            self.candidates[line_nb][col_nb] = set(element)
 
     def find_unique_candidates(self):
+        counter = 0
         for line_nb in range(9):
             for col_nb in range(9):
                 for candidate in self.candidates[line_nb][col_nb]:
+                    if self.sudoku[line_nb][col_nb].isdigit():
+                        continue
                     # horizontal scanning
                     other_candidates = -1
                     for i in range(9):
@@ -39,47 +42,47 @@ class SudokuSolver:
                             other_candidates += 1
                             if other_candidates > 0:
                                 continue
-                    if not other_candidates:
-                        self.sudoku[line_nb][col_nb] = candidate
-                        self.eliminate_aligned_candidates(line_nb, col_nb)
-                        break
-
-                    # vertical scanning
-                    other_candidates = -1
-                    for i in range(9):
-                        if candidate in self.candidates[i][col_nb]:
-                            other_candidates += 1
-                            if other_candidates > 0:
-                                continue
-                    if not other_candidates:
-                        self.sudoku[line_nb][col_nb] = candidate
-                        self.eliminate_aligned_candidates(line_nb, col_nb)
-                        break
-
-                    # section scanning
-                    other_candidates = -1
-                    for y in range(line_nb // 3 * 3, line_nb // 3 * 3 + 3):
-                        for x in range(col_nb // 3 * 3, col_nb // 3 * 3 + 3):
+                    if other_candidates:
+                        # vertical scanning
+                        other_candidates = -1
+                        for i in range(9):
                             if candidate in self.candidates[i][col_nb]:
                                 other_candidates += 1
                                 if other_candidates > 0:
                                     continue
+                    if other_candidates:
+                        # section scanning
+                        other_candidates = -1
+                        for y in range(line_nb // 3 * 3, line_nb // 3 * 3 + 3):
+                            for x in range(col_nb // 3 * 3, col_nb // 3 * 3 + 3):
+                                if candidate in self.candidates[i][col_nb]:
+                                    other_candidates += 1
+                                    if other_candidates > 0:
+                                        continue
                     if not other_candidates:
                         self.sudoku[line_nb][col_nb] = candidate
                         self.eliminate_aligned_candidates(line_nb, col_nb)
+                        counter += 1
                         break
 
-    def update(self):
+        if counter > 0:
+            print(f'{counter} position(s) updated.')
+            return True
+        print(f'No positions updated.')
+        return False
+
+    def find_single_candidates(self):
         counter = 0
         for line_nb in range(9):
             for col_nb in range(9):
-                if len(self.candidates[line_nb][col_nb]) == 1:
+                if len(self.candidates[line_nb][col_nb]) == 1 and self.sudoku[line_nb][col_nb] == '.':
                     counter += 1
-                    self.sudoku[line_nb][col_nb] = str(self.candidates[line_nb][col_nb].pop())
+                    element = next(iter(self.candidates[line_nb][col_nb]))
+                    self.sudoku[line_nb][col_nb] = str(element)
         if counter > 0:
-            print(f'{counter} digit(s) updated.')
+            print(f'{counter} position(s) updated.')
             return True
-        print(f'No digits updated.')
+        print(f'No positions updated.')
         return False
 
     def validate(self):
@@ -121,12 +124,14 @@ class SudokuSolver:
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    sudoku = SudokuSolver('tables\\sudoku01.txt')
+    sudoku = SudokuSolver('tables\\medium01.txt')
+    print(sudoku)
     sudoku.eliminate_all_possible_candidates()
     while True:
-        print('find unique')
-        sudoku.find_unique_candidates()
-        print('update')
-        if not sudoku.update():
+        unique_candidates_found = sudoku.find_unique_candidates()
+        print(sudoku)
+        single_candidates_found = sudoku.find_single_candidates()
+        print(sudoku)
+        if not unique_candidates_found and not single_candidates_found:
             break
     sudoku.validate()
