e9d2baa8347ee468f7fe816606969a06127aae75
Szczepan86 <szczepanski.mateusz@gmail.com>
2021-07-08 20:00:56 +0200

    Class created

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-def get_sudoku(filename):
-    sudoku_table = []
-    with open(filename, encoding='utf-8') as file:
-        for line in file:
-            row = [d for d in line if d.isdigit()]
-            sudoku_table.append(row)
-        return sudoku_table
-    return None
+class SudokuSolver:
+    def __init__(self, filename):
+        self.sudoku = []
+        with open(filename, encoding='utf-8') as file:
+            for line in file:
+                row = [d for d in line if d.isdigit() or d == '.']
+                self.sudoku.append(row)
 
-
-def print_sudoku(sudoku):
-    for line_nb, line in enumerate(sudoku):
-        if line_nb % 3 == 0 and line_nb < len(line) and line_nb:
-            print(21 * '-')
-        for col_nb, element in enumerate(line):
-            if col_nb % 3 == 0 and col_nb < len(line) and col_nb:
-                print('|', end=' ')
-            if element == '0':
-                element = '.'
-            print(element, end=' ')
-        print()
+    def __str__(self):
+        output = ''
+        for line_nb, line in enumerate(self.sudoku):
+            if line_nb % 3 == 0 and line_nb < len(line) and line_nb:
+                output += f'{21 * "-"}\n'
+            for col_nb, element in enumerate(line):
+                if col_nb % 3 == 0 and col_nb < len(line) and col_nb:
+                    output += f'| '
+                output += f'{element} '
+            output += f'\n'
+        return output
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    sudoku = get_sudoku('tables\\sudoku01.txt')
-    print_sudoku(sudoku)
+    sudoku = SudokuSolver('tables\\sudoku01.txt')
+    print(sudoku)
